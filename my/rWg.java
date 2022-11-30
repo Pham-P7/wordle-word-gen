142a114a6d3a01048bc1543e7f0276ff01991ef9
Phat Pham <101293853+Pham-P7@users.noreply.github.com>
2022-11-29 19:01:37 -0600

    Add files via upload

---
+++ my/rWg.java
@@ -0,0 +1,25 @@
+package my;
+import java.io.File;
+import java.util.Scanner;
+public class rWg
+{
+  public static String Gen() throws Exception
+  {
+    File file = new File("file location here");
+    Scanner sc = new Scanner(file);
+    String list[] = {};
+    while (sc.hasNextLine()){
+        list = add(list,sc.nextLine());
+    }
+    sc.close();
+    int pick = (int)(Math.random() * list.length);
+    return list[pick];
+  }
+  public static String[] add(String arr[],String n){
+    String[] list = new String[arr.length + 1];
+    for(int i = 0; i < arr.length; i++)
+        list[i] = arr[i];
+    list[list.length - 1] = n;
+    return list;
+  }
+}
